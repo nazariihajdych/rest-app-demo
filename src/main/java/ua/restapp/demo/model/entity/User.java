9d40baaccd21a83c8b745e132f2859380dd7b222
nazariihajdych <wertyp.poiteq@gmail.com>
2024-04-25 16:25:54 +0300

    add user entity

---
+++ src/main/java/ua/restapp/demo/model/entity/User.java
@@ -0,0 +1,30 @@
+package ua.restapp.demo.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "t_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "email", nullable = false)
+    private String email;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+    @Column(name = "date_of_birth", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date dateOfBirth;
+    private String phoneNumber;
+}
