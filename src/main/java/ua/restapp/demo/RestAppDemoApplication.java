908a1e4a8970e777795855507e723a2bc90c81d9
nazariihajdych <wertyp.poiteq@gmail.com>
2024-04-25 16:25:37 +0300

    add dependencies

--- src/main/java/ua/restapp/demo/RestAppDemoApplication.java
+++ src/main/java/ua/restapp/demo/RestAppDemoApplication.java
@@ -1,4 +1,4 @@
-package ua.restapp.demo.restappdemo;
+package ua.restapp.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
