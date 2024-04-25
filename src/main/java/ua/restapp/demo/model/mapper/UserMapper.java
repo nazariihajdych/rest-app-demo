c582b95ded66d510404d059d7224ccc12e5abd4e
nazariihajdych <wertyp.poiteq@gmail.com>
2024-04-25 21:34:00 +0300

    add mappers to update and partly update user

--- src/main/java/ua/restapp/demo/model/mapper/UserMapper.java
+++ src/main/java/ua/restapp/demo/model/mapper/UserMapper.java
@@ -1,6 +1,10 @@
 package ua.restapp.demo.model.mapper;
 
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
 import ua.restapp.demo.model.dto.UserDTO;
 import ua.restapp.demo.model.entity.User;
@@ -11,4 +15,12 @@ public interface UserMapper {
     UserDTO userToUserDTO(User user);
 
     User userDTOToUser(UserDTO userDTO);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", source = "id", ignore = true)
+    void partlyUpdateUserFromDTO(UserDTO userDTO, @MappingTarget User user);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL)
+    @Mapping(target = "id", source = "id", ignore = true)
+    void updateUserFromDTO(UserDTO userDTO, @MappingTarget User user);
 }
