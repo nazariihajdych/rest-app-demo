3fabdef1fb28c6a1ad424a065061cd94c660bbd6
nazariihajdych <wertyp.poiteq@gmail.com>
2024-04-25 21:35:14 +0300

    add service methods in endpoints

--- src/main/java/ua/restapp/demo/controller/UserController.java
+++ src/main/java/ua/restapp/demo/controller/UserController.java
@@ -2,13 +2,14 @@ package ua.restapp.demo.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ua.restapp.demo.model.dto.UserDTO;
 import ua.restapp.demo.service.UserService;
 
-import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -23,31 +24,30 @@ public class UserController {
         return new ResponseEntity<>(userService.createUser(userDTO), HttpStatus.CREATED);
     }
 
-    @PutMapping
-    public ResponseEntity<UserDTO> updateUser(@Valid @RequestBody UserDTO userDTO) {
-        return ResponseEntity.ok(null);
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDTO> updateUser(
+            @PathVariable Long id,
+            @Valid @RequestBody UserDTO userDTO) {
+        return ResponseEntity.ok(userService.updateUser(id, userDTO));
     }
 
-    @PatchMapping
-    public ResponseEntity<UserDTO> updateUserPartly(@Valid @RequestBody UserDTO userDTO){
-        return ResponseEntity.ok(null);
+    @PatchMapping("/{id}")
+    public ResponseEntity<UserDTO> updateUserPartly(
+            @PathVariable Long id,
+            @Valid @RequestBody UserDTO userDTO){
+        return ResponseEntity.ok(userService.partialUserUpdate(id, userDTO));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<UserDTO> deleteUser(@PathVariable Long id) {
-        return ResponseEntity.ok(null);
+        return ResponseEntity.ok(userService.deleteUser(id));
     }
 
     @GetMapping
     public ResponseEntity<List<UserDTO>> getUserByBirthDateRange(
-            @RequestParam(name = "from") Integer from,
-            @RequestParam(name = "to") Integer to){
-
-        ArrayList<UserDTO> userDTOS = new ArrayList<>();
-        UserDTO userDTO = new UserDTO();
-        userDTO.setFirstName("Hello");
-        userDTOS.add(userDTO);
-        return ResponseEntity.ok(userDTOS);
+            @RequestParam(name = "from") @DateTimeFormat(pattern="yyyy-MM-dd") Date from,
+            @RequestParam(name = "to") @DateTimeFormat(pattern="yyyy-MM-dd") Date to){
+        return ResponseEntity.ok(userService.getUserByBirthDateRange(from, to));
     }
 
 
