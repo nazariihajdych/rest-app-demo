402eb0faa3d2bd3113170cd47e216c3ea0c14d3c
nazariihajdych <wertyp.poiteq@gmail.com>
2024-04-25 23:53:14 +0300

    delete not required validation

--- src/main/java/ua/restapp/demo/model/dto/UserDTO.java
+++ src/main/java/ua/restapp/demo/model/dto/UserDTO.java
@@ -2,26 +2,20 @@ package ua.restapp.demo.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
 public class UserDTO {
     private Long id;
-    @NotNull
     @Email(regexp = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$",
             message = "not valid email")
     private String email;
-    @NotNull
     private String firstName;
-    @NotNull
     private String lastName;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    @NotNull
     @Past(message = "should be in past")
     private Date dateOfBirth;
     private String phoneNumber;
