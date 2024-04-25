ae26b0521e71b5e281fb7168248c283ad8a90bbe
nazariihajdych <wertyp.poiteq@gmail.com>
2024-04-25 21:36:13 +0300

    delete unnecessary files

--- src/main/java/ua/restapp/demo/exception/InconsistencyException.java
+++
@@ -1,7 +0,0 @@
-package ua.restapp.demo.exception;
-
-public class InconsistencyException extends RuntimeException{
-    public InconsistencyException(String message) {
-        super(message);
-    }
-}
